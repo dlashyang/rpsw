1cfeb8c980221791274ef9e4e3ca740d95587d42
dlashyang <dlashyang@gmail.com>
2016-05-11 22:02:46 +0800

    use auto ptr to manege resource

--- rpswd.cc
+++ rpswd.cc
@@ -13,9 +13,8 @@ int main()
     std::cout<<"This is rpswd."<<std::endl;
 
     dummy_factory d("phalanx");
-    cmm_hw_res* hw = d.create_hw_res();
+    std::auto_ptr<cmm_hw_res> hw(d.create_hw_res());
     hw->init();
-    std::auto_ptr<cmm_hw_res> _hw_just_for_gc(hw);
 
 #if 0
     rpsw_event_handler evnt_hdlr;
@@ -23,13 +22,12 @@ int main()
     evnt_hdlr.start();
 #endif
 
-    rpsw_msg_dispatcher* dsp=simple_msg_disp_factory(hw).create_dispatcher();
-    rpsw_event_server* evnt_srv=poco_server_factory(dsp).create_server();
-    std::auto_ptr<rpsw_event_server> _srv_just_for_gc(evnt_srv);
+    std::auto_ptr<rpsw_msg_dispatcher> dsp(simple_msg_disp_factory(hw.get()).create_dispatcher());
+    std::auto_ptr<rpsw_event_server> evnt_srv(poco_server_factory(dsp.get()).create_server());
     evnt_srv->init();
     evnt_srv->start();
 
-    rpsw_fault_scanner fault_scanner(hw);
+    rpsw_fault_scanner fault_scanner(hw.get());
     fault_scanner.start();
 
     //for debug
